d10a8e931c56ad89dcbd579cad4027940e37d410
dongasai <1514582970@qq.com>
2019-05-08 16:11:19 +0800

    修正一处BUG

--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -14,11 +14,13 @@
                     <div class="hist-list" v-if="hist">
                       项目列表:
                       <br><br>
-                      <ul>
-                          <li v-for="hi in hist" @click="open_obj(hi)">
+                      <div> 
+
+                      </div>
+                          <div class="hist-li" v-for="hi in hist" @click="open_obj(hi)">
                               {{hi}}
-                          </li>
-                      </ul>
+                          </div>
+                      
                   </div>
                 </div>
 
@@ -112,8 +114,8 @@
       font-size: 2em;
 
     }
-    .hist-list li{
-      display:inline;line-height:24px;height:auto;word-break:break-all;word-wrap : break-word ;
+    .hist-li{
+      display:inline;line-height:24px;height:auto;word-wrap : break-word ;
     }
     #holder {
         padding: 30px;
